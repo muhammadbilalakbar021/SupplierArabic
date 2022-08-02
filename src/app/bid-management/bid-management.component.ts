32afebdfc60bb24553ed528ce312d4c3ee0cc9e6
AbdurRehmanOnly1 <abdur@whydonate.nl>
2022-08-02 23:27:27 +0500

    Added Bid

--- src/app/bid-management/bid-management.component.ts
+++ src/app/bid-management/bid-management.component.ts
@@ -6,10 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bid-management.component.css']
 })
 export class BidManagementComponent implements OnInit {
-
+  public loadmore: boolean = false
+  public showVar : string = "Show More"
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  show() {
+    if (this.loadmore) {
+      this.showVar = "Hide";
+    } else {
+      this.showVar = "Show More";
+    }
+    this.loadmore = !this.loadmore;
+    console.log("Show More",this.loadmore);
+  }
 }
